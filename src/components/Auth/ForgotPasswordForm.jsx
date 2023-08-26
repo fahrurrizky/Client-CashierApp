969763857bd823c139140a10e65ea2c4b8f8c45d
M Fahrur Rizky <70459040+fahrurrizky@users.noreply.github.com>
2023-08-26 22:39:09 +0700

    useState

--- src/components/Auth/ForgotPasswordForm.jsx
+++ src/components/Auth/ForgotPasswordForm.jsx
@@ -14,6 +14,7 @@ import {
   Link as LinkChakra,
   Spinner,
 } from "@chakra-ui/react";
+import React, {useState} from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import axios from "axios";
